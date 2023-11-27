683b88f5276949c84d980481123c6a801dddfa39
Silent Crafter <63489080+Silent-Crafter@users.noreply.github.com>
2023-11-27 11:59:59 +0530

    Changes look and added color picking

--- paint.cpp
+++ paint.cpp
@@ -15,21 +15,20 @@ private:
 	int boxH = 20;
 	int boxW = 20;
 
-	int startx = 50;
+	int startx = 40;
 	int starty = 100;
 
 	int padding = 5;
-
-	int colors[MAXCOLORS+1] = { BLACK, BLUE, GREEN, CYAN,
-							  RED, MAGENTA, BROWN, LIGHTGRAY,
-							  DARKGRAY, LIGHTBLUE, LIGHTGREEN, LIGHTCYAN,
-							  LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE };
+	
+	int colors[14] = { BLACK, BLUE, GREEN, CYAN,
+					 RED, MAGENTA, BROWN, LIGHTBLUE, 
+					 LIGHTGREEN, LIGHTCYAN, LIGHTRED, LIGHTMAGENTA, 
+					 YELLOW, DARKGRAY };
 
 public:
 
 	void render();
-	inline int getColor(int, int);
-	int clickEvent(int, int);
+	int getColor(int, int);
 };
 
 void ColorPallete::render() {
@@ -44,6 +43,7 @@ void ColorPallete::render() {
 						xpos, ypos+boxH, 
 						xpos, ypos };
 
+		setcolor(BLACK);
 		drawpoly(5, points);
 		setfillstyle(SOLID_FILL, colors[i]);
 		fillpoly(5, points);
@@ -60,12 +60,18 @@ void ColorPallete::render() {
 	}
 }
 
-int ColorPallete::clickEvent(int mx, int my) {
-	return getColor(mx, my);
-}
 
-inline int ColorPallete::getColor(int mx, int my) {
-	return getpixel(mx, my);
+int ColorPallete::getColor(int mx, int my) {
+	int c = getpixel(mx, my);
+	int cols = 2;
+	int rows = sizeof(colors)/sizeof(colors[0])/cols + 1;
+
+	// Check if its inside the box
+	if ( ( mx > startx && mx < (startx + boxW*cols + padding) ) &&
+		( my > starty && my < (starty + (boxH + padding)*rows) ) )
+		return c;
+	else
+		return -1;
 }
 
 
@@ -75,6 +81,8 @@ private:
 	int startx, starty;
 	
 	int points[10] = {0};
+
+	int canvasColor = WHITE;
 
 	void initPoints() {
 		points[0] = startx; 
@@ -97,11 +105,20 @@ public:
 
 	inline void render(int x = 100, int y = 100) {
 		drawpoly(5, points);
+		setfillstyle(SOLID_FILL, canvasColor);
+		fillpoly(5, points);
 	}
 
 	inline void clear() {
-		setfillstyle(SOLID_FILL, BLACK);
+		setfillstyle(SOLID_FILL, canvasColor);
 		fillpoly(5, points);
+	}
+
+	inline bool canDraw(int x, int y) {
+		return ( ( x > startx && x < startx + width ) && 
+			( y > starty && y < starty + height ) ) 
+			? true 
+			: false;
 	}
 
 	friend class PaintApp;
@@ -112,16 +129,15 @@ class Pen {
 private:
 	int penColor;
 	int penSize;
-
-public:
 	bool penDown;
 	bool eraser;
 
-	Pen() : penColor(WHITE), penDown(false), eraser(false), penSize(10) {}
+public:
+	Pen() : penColor(BLACK), penDown(false), eraser(false), penSize(10) {}
 
 	
 	inline void togglePen() { penDown = ( penDown ) ? false : true; }
-	inline void toggleEraser() { eraser = (eraser) ? false : true; penColor = ( penColor == BLACK ) ? WHITE : BLACK; }
+	inline void toggleEraser(int c) { eraser = (eraser) ? false : true; penColor = ( penColor == c ) ? BLACK : c; }
 
 	inline int getColor() { return penColor; }
 	inline void setColor(int c) { penColor = c; }
@@ -141,55 +157,63 @@ public:
 		drawpoly(5, points);
 		fillpoly(5, points);
 	}
+
+	friend class PaintApp;
 };
 
 
 class PaintApp {
 private:
+	int width = 1280;
+	int height = 720;
+
 	int padding = 5;
 
-	typedef enum status { EXIT, CONTINUE, CLEAR_CANVAS } status;
+	int backgroundColor = DARKGRAY;
+
+	typedef enum status { EXIT, CONTINUE, DRAW, CLEAR_CANVAS } status;
+
+	int mx, my;
 
 	status s = CONTINUE;
 
 	Canvas* canvas;
 	ColorPallete* cp;
-
 	Pen* pen;
 
 public:
 	PaintApp() {
-		canvas = new Canvas();
-		cp = new ColorPallete();
-		pen = new Pen();
+		canvas = new Canvas;
+		cp = new ColorPallete;
+		pen = new Pen;
+		shapes = new Shapes;
 	}
 	
 	inline void render() {
+		int points[] = {
+			0, 0,
+			width, 0,
+			width, height,
+			0, height,
+			0, 0
+		};
+		setfillstyle(SOLID_FILL, backgroundColor);
+		fillpoly(5, points);
 		canvas->render();
 		cp->render();
 	}
 	
-	inline void resetColor() { setcolor(WHITE); };
+	inline void resetColor() { setcolor(canvas->canvasColor); };
 
 	void mainloop() {
-		initwindow(1280, 720, "CG PROJECT : Paint App");
+		initwindow(width, height, "CG PROJECT : Paint App");
 
 		render();
 
 		do {
-			int mx, my;
-
-			mx = mousex();
-			my = mousey();
-
-
-			if ( pen->penDown && ( mx > canvas->startx && mx < canvas->startx + canvas->width ) && ( my > canvas->starty && my < canvas->starty + canvas->height ) ) {
-				pen->draw(mx, my);
-			}
-
+			handleMouse();
 			handleKeys();
 			handleStatus();
-			setcolor(WHITE);
 		} while( s );
 
 		closegraph();
@@ -211,7 +235,7 @@ public:
 			break;
 
 		case 101:
-			pen->toggleEraser();
+			pen->toggleEraser(canvas->canvasColor);
 			break;
 
 		case 113:
@@ -226,12 +250,36 @@ public:
 	void handleStatus() {
 		switch ( s ) {
 		case CLEAR_CANVAS:
-			canvas->clear();
-			s = CONTINUE;
+			clearviewport();
+			render();
+			break;
+
+		case DRAW:
+			pen->draw(mx, my);
 			break;
 
 		default:
 			break;
+		}
+		if (s != EXIT) s = CONTINUE;
+	}
+
+	void handleMouse() {
+		bool clicked = ismouseclick(WM_LBUTTONDOWN);
+		clearmouseclick(WM_LBUTTONDOWN);
+
+		mx = mousex();
+		my = mousey();
+
+		if ( pen->penDown && canvas->canDraw(mx, my) ) {
+			s = DRAW;
+			return;
+		}
+
+		if (clicked) {
+			int c = cp->getColor(mx, my);
+			if ( c != -1 )
+				pen->setColor(c);
 		}
 	}
 
